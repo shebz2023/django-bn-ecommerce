779d1bb3a5180f8226b3059eaa19e3ed2b96d6c7
shebz2023 <shebelleke@gmail.com>
2024-09-21 23:39:08 +0200

    registered models to the admin site

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["title"]
+    list_per_page = 5
+
+
+@admin.register(models.Customer)
+class CustomerModel(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "membership"]
+    list_editable = ["membership"]
+    list_per_page = 10
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "unit_price","inventory"]
+    list_editable = ["unit_price"]
+    list_per_page = 10
