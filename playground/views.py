1fbd04c7a5b3298235d3338921bbbfbfc04df44c
shebz2023 <shebelleke@gmail.com>
2024-09-22 00:36:02 +0200

    started adding REST apis

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from store.models import Product
+
 
 def say_hello(request):
-    queryset = Product.objects.all()[:5]
-    return render(request, "hello.html", {"name": "Mosh", "products": list(queryset)})
+    return render(request, 'hello.html', {'name': 'Mosh'})
