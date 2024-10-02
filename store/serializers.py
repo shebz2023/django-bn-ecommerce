137547439b5cc05f5fb512171ad99964ba50706d
shebz2023 <shebelleke@gmail.com>
2024-10-02 13:45:20 +0200

    custom serializers

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,16 @@
-from rest_framework import Serializers
+from decimal import Decimal
+from rest_framework import serializers
+from .models import Product
 
 
-class productSerializer(Serializers.Serializers):
+class productSerializer(serializers.Serializer):
 
-    id = Serializers.IntegerField()
-    title = Serializers.CharField(max_length=255)
-    unit_price = Serializers.DecimalFields(max_digits=6, decimal_places=2)
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    price = serializers.DecimalField(
+        max_digits=6, decimal_places=2, source="unit_price"
+    )
+    price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import productSerializer
 
 # Create your views here.
 
 
 @api_view()
 def product_list(request):
-    return Response("ok")
+    products = Product.objects.all()
+    serializer = productSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def singleProduct(request, id):
-    return Response("ok")
+    try:
+        product = Product.objects.get(id=id)
+        serializer = productSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response({"error": "Product not found"}, status=404)
